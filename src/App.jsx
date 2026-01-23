b252a590fdaa619d6070f7414151eeaad8843585
SALIK-JAVID <salikjaveeddar@gmail.com>
2026-01-23 23:45:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="font-bold font-zentry text-violet-300">
+      Xentra : the center for extreme gaming
+    </div>
+  )
+}
+
+export default App
+
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          zentry: ["zentry", "sans-serif"],
+          general: ["general", "sans-serif"],
+          circular: ["circular-web", "sans-serif"],
+          robert: ["robert-regular", "sans-serif"],
+          "robert-medium": ["robert-medium", "sans-serif"],
+        },
+        colors: {
+            blue:{
+              50: '#DFDFF0',
+              75: '#DFDFF2',
+              100: '#F0F2FA',
+              200:'#101010',
+              300:'#4FB7DD'
+            },
+            violet:{
+                300:'#5724FF',
+
+            },
+            yellow:{
+                100:'#8E983F',
+                300:'#EDFF66'
+            }
+        }
+        
+      },
+    },
+  };
+  
