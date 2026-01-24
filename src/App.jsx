7afee7a5e7419c04323a09be234e3a3962ed5005
SALIK-JAVID <salikjaveeddar@gmail.com>
2026-01-24 10:36:25 +0530

    Added all the required fonts and colors for the project

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ const App = () => {
   return (
     <div className="font-bold font-zentry text-violet-300">
       Xentra : the center for extreme gaming
+      <div className='text-[color:var(--test-color)]'>
+       hello world
+      </div>
     </div>
   )
 }
